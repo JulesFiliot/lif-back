83951642fa57ebce068d74566cc58bb5c0f6a789
PierreCruveiller <pierre.cruveiller@cpe.fr>
2023-01-09 11:09:46 +0100

    [ADD] Added route to get threads from subcategory;

--- thread_microservice/api/route.js
+++ thread_microservice/api/route.js
@@ -5,7 +5,8 @@ const bodyParser = require("body-parser");
 let router = express.Router();
 router.use(bodyParser.json());
 
-router.get("/get-threads", Controller.getThreads);
+router.get("/threads", Controller.getThreads);
+router.get("/threads/:subcat_id", Controller.getSubcatThreads);
 router.post("/create-thread", Controller.createThread);
 router.post("/:thread_id/vote", Controller.voteThread);
 
