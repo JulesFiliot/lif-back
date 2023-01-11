d356920d8b97e216dc0947c4f64f3569c86d1b9c
PierreCruveiller <pierre.cruveiller@cpe.fr>
2023-01-11 15:15:52 +0100

    [ADD] added created_at attribute on subcategory creation;

--- category_microservice/dto/subcategory-dto.js
+++ category_microservice/dto/subcategory-dto.js
@@ -2,6 +2,7 @@ class SubcategoryDTO {
     constructor(name, parent_cat_id) {
         this.name = name;
         this.parent_cat_id = parent_cat_id;
+        this.created_at = new Date().toISOString();
     }
   }
   
