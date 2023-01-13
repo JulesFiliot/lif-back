f11b717c874114565381f69fd276da380dc79415
PierreCruveiller <pierre.cruveiller@cpe.fr>
2023-01-13 12:14:38 +0100

    [CHANGE] changed user api route names;

--- user_microservice/api/route.js
+++ user_microservice/api/route.js
@@ -7,12 +7,12 @@ let upload = multer();
 
   
 
-router.get("/get-user/:id", upload.array(), Controller.getUser);
-router.get("/get-all-users/", upload.array(), Controller.getAllUsers);
-router.post("/add-user/", Controller.addUser);
+router.get("/user/:id", upload.array(), Controller.getUser);
+router.get("/users/", upload.array(), Controller.getAllUsers);
+router.post("/user/", Controller.addUser);
 router.post("/edit-bio/", Controller.editBio);
 router.post("/remove-user-achievement/", Controller.removeUserAchievement);
-router.post("/add-user-achievement/", Controller.addUserAchievement);
+router.post("/user-achievement/", Controller.addUserAchievement);
 router.get("/valid-user-count/:subcat_id",Controller.getValidUserCount);
 //router.post('/notify', Controller.notif);
 module.exports = router;
